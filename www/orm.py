f8f176062e246f6edee626a1be8d5b0325201c91
fanglijuan <944974531@qq.com>
2018-05-16 18:07:10 +0800

    web工程-ORM -day2

--- www/orm.py
+++ www/orm.py
@@ -1,7 +1,10 @@
 import asyncio
 import logging;logging.basicConfig(level=logging.INFO)
 import aiomysql
+import
 
+def log(sql,args=()):
+    logging.info('SQL:%s'%sql)
 
 @asyncio.coroutine
 #创建连接池
@@ -24,7 +27,7 @@ def create_pool(loop,**kw):
 #执行select语句
 @asyncio.coroutine
 def select(sql,args,size=None):
-    logging.info(sql,args)
+    log(sql,args)
     global __pool
     with (yield from __pool) as conn:
         cur = yield from conn.cursor(aiomysql.DictCursor)
@@ -40,7 +43,7 @@ def select(sql,args,size=None):
 #要执行INSERT、UPDATE、DELETE语句，可以定义一个通用的execute()函数
 @asyncio.coroutine
 def execute(sql,args):
-    logging.info(sql)
+    log(sql)
     with(yield from __pool) as conn:
         try:
             cur = yield from conn.cursor()
@@ -50,5 +53,73 @@ def execute(sql,args):
         except BaseException as e:
             raise
         return affected
+
+#将数个数用逗号连接
+def create_args_string(num):
+    L =[]
+    for i in range(num):
+        L.append('?')
+    return ', '.join(L)
+
+class Field(object):
+    def __init__(self,name,column_type,primary_key,default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+
+    def __str__(self):
+        return '<%s, %s:%s>'%(self.__class__.__name__,self.column_type,self.name)
+
+class StringField(Field):
+    def __init__(self,name=None,primary_key=False,default=None,ddl='varchar(100'):
+        super().__init__(name,ddl,primary_key,default)
+
+class BooleanField(Field):
+    def __init__(self,name=None,default = False):
+        super().__init__(name,'boolean',False,default)
+
+class IntegetField(Field):
+    def __init__(self,name=None,primary_key=False,default=0):
+        super().__init__(name,'bigint',primary_key,default)
+
+class FloatField(Field):
+    def __init__(self,name=None,primary_key=False,default=0.0):
+        super().__init__(name,'real',primary_key,default)
+
+class TextField(Field):
+    def __int__(self,name=None,default=None):
+        super().__init__(name,'text',False,default)
+
+class ModelMetaclass(type):
+    def __new__(cls,name,bases,attrs):
+        if name =='Model':
+            return type.__new__(cls,name,bases,attrs)
+        tableName = attrs.get('__table__',None) or name
+        logging.info('found model: %s(table: %s)'%(name,tableName))
+        mappings = dict()
+        fields = []
+        primaryKey = None
+        for k,v in attrs.items:
+            if isinstance(v,Field):
+                logging.info('  found mapping: %s ==> %s'%(k,v))
+                mappings[k] = v
+                if v.primary_key:
+                    #找到主键
+                    if primaryKey:
+                        raise BaseException ('Duplicate primary key for field: %s'%k)
+                    primaryKey = k
+                else:
+                    fields.append(k)
+        if not primaryKey:
+            raise BaseException('Primary key not found')
+        for k in mappings.keys():
+            attrs.pop(k)
+        escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+        attrs['__mappings__'] = mappings  #保存属性和列的映射关系
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] =primaryKey #主键属性名
+        # attrs['__field__']
+
 
 
