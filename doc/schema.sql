f2d3194b4e47cb6272106bf3e2cd2bd2011a1541
fanglijuan <944974531@qq.com>
2018-05-17 17:13:27 +0800

    web工程-Model入库 -day3

--- doc/schema.sql
+++ doc/schema.sql
@@ -4,7 +4,7 @@ create database awesome;
 
 use awesome;
 
-grant select, insert, update, delete on awesome.* to 'www-data'@'localhost' identified by 'www-data';
+grant select, insert, update, delete on mysql.* to 'root'@'localhost' identified by '123456aa';
 
 create table user1 (
     `id` varchar(50) not null,
